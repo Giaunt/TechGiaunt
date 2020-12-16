724ab8f1f2826cd14f83c6a4225f6d613045c34f
Erica <Giaunt@users.noreply.github.com>
2020-12-15 22:34:48 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -242,5 +242,11 @@
   $(window).on('load', function() {
     aos_init();
   });
-
+var quadimages = document.querySelectorAll("#quad figure");
+for (i=0; i<quadimages.length; i++) {
+	quadimages[i].addEventListener( 'click',  function() {
+		this.classList.toggle("expanded");
+		quad.classList.toggle("full");
+	});
+}
 })(jQuery);
